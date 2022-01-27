3fe46e025eec20c1b192743a1c05837cf831d086
Gabriel <gabriel.s.rosin@gmail.com>
2022-01-26 21:52:59 -0300

    First models created, still have to test and improve them

--- biolab_simulator/density/admin.py
+++ biolab_simulator/density/admin.py
@@ -1,3 +1,54 @@
+from operator import mod
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+class EquationsInline(admin.TabularInline):
+    model = models.Equations
+    extra = 1
+
+
+class GraphsInline(admin.TabularInline):
+    model = models.Graphs
+    extra = 1
+
+
+class CompoundsInline(admin.TabularInline):
+    model = models.Compound
+    extra = 1
+
+
+class ParametersInline(admin.TabularInline):
+    model = models.Parameters
+    extra = 1
+
+
+class AbsoluteParameterInline(admin.TabularInline):
+    model = models.AbsoluteParameter
+    extra = 1
+
+
+class PredictiveModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [
+        EquationsInline,
+        GraphsInline,
+        CompoundsInline,
+        AbsoluteParameterInline
+    ]
+
+
+class CompoundAdmin(admin.ModelAdmin):
+    list_display = ['getModel', 'name']
+    inlines = [
+        ParametersInline
+    ]
+
+
+admin.site.register(models.PredictiveModel, PredictiveModelAdmin)
+admin.site.register(models.Compound, CompoundAdmin)
+admin.site.register(models.Equations)
+admin.site.register(models.Graphs)
+admin.site.register(models.AbsoluteParameter)
+admin.site.register(models.Parameters)
--- biolab_simulator/density/models.py
+++ biolab_simulator/density/models.py
@@ -1,3 +1,56 @@
+from operator import mod
+from pyexpat import model
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class PredictiveModel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50 ,verbose_name="Model's name")
+    description = models.CharField(max_length=10000 ,verbose_name="Model's description")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Equations(models.Model):
+    model_id = models.ForeignKey(PredictiveModel, on_delete=models.CASCADE)
+    number = models.IntegerField(verbose_name="Equation's number", blank=True)
+    equation = models.ImageField(verbose_name="Equation's image")
+
+
+class Graphs(models.Model):
+    model_id = models.ForeignKey(PredictiveModel, on_delete=models.CASCADE)
+    label = models.CharField(max_length=100 ,verbose_name="Graph's label", blank=True)
+    image = models.ImageField(verbose_name="Graph's image")
+
+    def __str__(self) -> str:
+        return self.label
+
+
+class AbsoluteParameter(models.Model):
+    model_id = models.ForeignKey(PredictiveModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50 ,verbose_name="Parameter's name", blank=True)
+    value = models.FloatField(verbose_name="Parameter's value", blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Compound(models.Model):
+    model_id = models.ForeignKey(PredictiveModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50 ,verbose_name="Compound's representation", blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def getModel(self) -> str:
+        return self.model_id.name
+
+class Parameters(models.Model):
+    model_id = models.ForeignKey(Compound, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50 ,verbose_name="Parameter's name")
+    value = models.FloatField(verbose_name="Parameter's value")
+
+    def __str__(self) -> str:
+        return self.name
