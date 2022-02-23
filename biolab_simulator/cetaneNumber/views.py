d9f94df317ef5a0dfad247a666da5a55ba21090d
Gabriel <gabriel.s.rosin@gmail.com>
2022-02-23 20:36:02 -0300

    Setting up cetane number's views and template

--- biolab_simulator/cetaneNumber/views.py
+++ biolab_simulator/cetaneNumber/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView
+from . import models
 
 # Create your views here.
 def cetaneNumber(request):
     template_name = 'cetaneNumber/cetaneNumber.html'
     return render(request, template_name)
+
+
+class PredictiveModel(DetailView):
+    model = models.PredictiveModel
+    template_name = 'cetaneNumber/model.html'
+    context_object_name = 'model'
+
+    def get_object(self):
+        self.model = get_object_or_404(models.PredictiveModel,
+        name=self.kwargs['name'])
+        return self.model
