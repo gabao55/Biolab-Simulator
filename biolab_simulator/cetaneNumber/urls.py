90aa1c04a621a3eecb4a7a60c9290b4aec0d2d9a
Gabriel <gabriel.s.rosin@gmail.com>
2022-02-23 20:54:45 -0300

    Setting up urls

--- biolab_simulator/cetaneNumber/urls.py
+++ biolab_simulator/cetaneNumber/urls.py
@@ -5,5 +5,5 @@ app_name = 'cetaneNumber'
 
 urlpatterns = [
     path('', views.cetaneNumber, name='cetaneNumber'),
-    path('<str:name>/', views.PredictiveModel.as_view(), name='cetane_number_model'),
+    path('<str:name>/', views.PredictiveModel.as_view(), name='cetaneNumber_model'),
 ]
--- biolab_simulator/pluggingPoint/urls.py
+++ biolab_simulator/pluggingPoint/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'pluggingPoint'
 
 urlpatterns = [
-    path('', views.pluggingPoint, name='pluggingPoint')
+    path('', views.pluggingPoint, name='pluggingPoint'),
+    path('<str:name>/', views.PredictiveModel.as_view(), name='pluggingPoint_model'),
 ]
