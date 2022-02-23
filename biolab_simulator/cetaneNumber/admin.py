01df07303f7cf71fc78c028f5dc4a7eb651ea014
Gabriel <gabriel.s.rosin@gmail.com>
2022-02-23 20:33:11 -0300

    Setting up cetane number's admin site

--- biolab_simulator/cetaneNumber/admin.py
+++ biolab_simulator/cetaneNumber/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from . import models
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+class EquationsInline(admin.TabularInline):
+    list_display = ['getModel', 'number']
+    model = models.Equation
+    extra = 1
+
+
+class GraphsInline(admin.TabularInline):
+    model = models.Graph
+    extra = 1
+
+
+class CompoundsInline(admin.TabularInline):
+    model = models.Compound
+    extra = 1
+
+
+class ParametersInline(admin.TabularInline):
+    model = models.Parameter
+    extra = 1
+
+
+class PredictiveModelAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+    list_display = ['name']
+    inlines = [
+        EquationsInline,
+        GraphsInline,
+        CompoundsInline,
+    ]
+
+
+class CompoundAdmin(admin.ModelAdmin):
+    list_display = ['getModel', 'name']
+    inlines = [
+        ParametersInline
+    ]
+
+
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ['getCompound', 'name', 'value']
+
+
+class GraphAdmin(admin.ModelAdmin):
+    list_display = ['getModel', 'label']
+
+admin.site.register(models.PredictiveModel, PredictiveModelAdmin)
+admin.site.register(models.Compound, CompoundAdmin)
+admin.site.register(models.Equation)
+admin.site.register(models.Graph, GraphAdmin)
+admin.site.register(models.Parameter, ParameterAdmin)
