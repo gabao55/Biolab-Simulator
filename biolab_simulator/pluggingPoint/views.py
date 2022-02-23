af420f5f172d6b5af3f92fa3f9cda80bfe2f2a43
Gabriel <gabriel.s.rosin@gmail.com>
2022-02-23 20:53:05 -0300

    Setting up plugging point's views

--- biolab_simulator/pluggingPoint/views.py
+++ biolab_simulator/pluggingPoint/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView
+from . import models
 
 def pluggingPoint(request):
     template_name = 'pluggingPoint/pluggingPoint.html'
     return render(request, template_name)
+
+
+class PredictiveModel(DetailView):
+    model = models.PredictiveModel
+    template_name = 'density/model.html'
+    context_object_name = 'model'
+
+    def get_object(self):
+        self.model = get_object_or_404(models.PredictiveModel,
+        name=self.kwargs['name'])
+        return self.model
