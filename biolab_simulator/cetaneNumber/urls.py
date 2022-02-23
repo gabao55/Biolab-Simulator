ce230cff9b57df39b7571e89dd022d34e0b2bc9e
Gabriel <gabriel.s.rosin@gmail.com>
2022-02-23 20:40:50 -0300

    Setting up cetane number's urls

--- biolab_simulator/cetaneNumber/urls.py
+++ biolab_simulator/cetaneNumber/urls.py
@@ -5,4 +5,5 @@ app_name = 'cetaneNumber'
 
 urlpatterns = [
     path('', views.cetaneNumber, name='cetaneNumber'),
+    path('<str:name>/', views.PredictiveModel.as_view(), name='cetane_number_model'),
 ]
