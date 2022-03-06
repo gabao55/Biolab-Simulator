661c306b3d37e7b2b467cdec7e12b13b02afa9fa
Gabriel <gabriel.s.rosin@gmail.com>
2022-03-06 09:52:45 -0300

    Displaying predictive models in the Home page

--- biolab_simulator/home/views.py
+++ biolab_simulator/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpRequest
 from django.views import View
 from . import models
+from density.models import PredictiveModel as DensityModels
 
 
 class Home(View):
@@ -9,8 +10,10 @@ class Home(View):
 
     def get(self, request, *args, **kwargs):
         home = models.Home.objects.filter(id='1').first()
+        density_models = DensityModels.objects.all
         # TODO: Add a query to get all the predictive models associated to each prop
-        context = {'home': home}
+        context = {'home': home,
+        'density_models': density_models}
 
         return render(request, self.template_name, context)
 
