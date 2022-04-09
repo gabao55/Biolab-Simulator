c6a65efb57dd1eb525a44c7b0051f4b3a01049f6
Gabriel <gabriel.s.rosin@gmail.com>
2022-04-09 18:00:17 -0300

    Removes unused View Class

--- biolab_simulator/density/views.py
+++ biolab_simulator/density/views.py
@@ -20,59 +20,6 @@ class Density(View):
         return render(request, self.template_name, context)
 
 
-#Template I was using before, but it's not needed
-#TODO: Remove class PredictiveModel
-class PredictiveModel(DetailView):
-    model = models.PredictiveModel
-    template_name = 'density/model.html'
-    
-    def get(self, request, *args, **kwargs):
-        self.model = get_object_or_404(models.PredictiveModel,
-        name=self.kwargs['name'])
-        print(self.kwargs['name'])
-
-        self.context = {
-            'model': self.model,
-        }
-
-        return render(request, self.template_name, self.context)
-
-    def post(self, request, *args, **kwargs):
-        self.model = self.model.objects.get(name=self.kwargs['name'])
-        if request.method == 'POST':
-            form = request.POST.dict()
-            compounds = {}
-            sum = 0
-            for compound, value in form.items():
-                if compound != 'csrfmiddlewaretoken' and value and int(value):
-                    compounds[compound] = {'composition': int(value)}
-                    sum += int(value)
-
-                    parameters_set = self.model.compound_set.get(esther_type=compound.split(' ')[0], name=compound.split(' ')[1]).parameter_set.all()
-
-                    for parameter in parameters_set:
-                        compounds[compound][parameter.name] = parameter.value
-
-            context = {
-                'compounds': compounds,
-            }
-            
-            if not compounds:
-                messages.error(self.request,
-                "Please insert parameters to predict property")
-
-                return self.get(request, self.template_name, name=self.kwargs['name'])
-
-            elif sum != 100:
-                messages.warning(self.request,
-                "The sum of compounds' mass percentage is not equal to 100%, this might affect the property predicted.")
-            else:
-                messages.success(self.request,
-                "Property predicted successfully.")
-
-            return render(request, 'density/predicted_property.html', context)
-
-
 class MurnaghanEquation(DetailView):
     template_name = 'density/model.html'
     model = get_object_or_404(models.PredictiveModel,
