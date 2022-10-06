7da75783ae12961594e18339ddde9d467036ab29
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-06 11:50:13 -0300

    Tries to fix static files

--- biolab_simulator/settings.py
+++ biolab_simulator/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS += ('django_summernote',)
 X_FRAME_OPTIONS = 'DENY'
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,8 +134,6 @@ STATICFILES_DIRS = [
     ]
 MEDIA_ROOT = BASE_DIR / 'templates/static/assets/img'
 MEDIA_URL = 'img/'
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
