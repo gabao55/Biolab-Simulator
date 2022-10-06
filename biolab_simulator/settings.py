c59641ac838a44701a41a444e97a166c38568bc2
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-06 11:23:56 -0300

    Fixes settings

--- biolab_simulator/settings.py
+++ biolab_simulator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://blsim.herokuapp.com/", "127.0.0.1", "0.0.0.0:5000"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
