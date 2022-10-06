e219586137ca782593e8b5103179ea57870ee1c1
gabao55 <gabriel.s.rosin@gmail.com>
2022-10-06 12:07:05 -0300

    Tries to fix static files

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn biolab_simulator.wsgi
+python manage.py collectstatic --noinput;
--- biolab_simulator/settings.py
+++ biolab_simulator/settings.py
@@ -135,8 +135,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'templates/static/assets/img'
 MEDIA_URL = 'img/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
